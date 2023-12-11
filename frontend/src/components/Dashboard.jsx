6736f93a394945365bb3201ba7294719a6bd68e7
Fatjri Zikri <fatjrizikri013@gmail.com>
2023-12-11 13:43:10 +0700

    fixed

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -10,18 +10,14 @@ const Dashboard = () => {
   const [users, setUsers] = useState([]);
   const navigate = useNavigate();
 
-  const getUsers = async () => {
-    const response = await axios.get('http://localhost:5000/users', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    setUsers(response.data);
-  };
-
   useEffect(() => {
-    const refreshToken = async () => {
-      try {
+    refreshToken();
+    getUsers();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const refreshToken = async () => {
+    try {
         const response = await axios.get('http://localhost:5000/token');
         setToken(response.data.accessToken);
         const decoded = jwtDecode(response.data.accessToken);
@@ -32,22 +28,18 @@ const Dashboard = () => {
           navigate('/');
         }
       }
-    };
+    }
     const axiosJWT = axios.create();
-    axiosJWT.interceptors.request.use(
-      async (config) => {
+
+    axiosJWT.interceptors.request.use(async (config) => {
         const currentDate = new Date();
         if (expired * 1000 < currentDate.getTime()) {
-          try {
             const response = await axios.get('http://localhost:5000/token');
             config.headers.Authorization = `Bearer ${response.data.accessToken}`;
             setToken(response.data.accessToken);
             const decoded = jwtDecode(response.data.accessToken);
             setName(decoded.name);
             setExpired(decoded.exp);
-          } catch (error) {
-            console.error('Error refreshing token:', error);
-          }
         }
         return config;
       },
@@ -56,12 +48,16 @@ const Dashboard = () => {
       }
     );
     
-
-    refreshToken();
-    getUsers(); // Call the function here
-
-  }, [token, expired, navigate]);
-
+    const getUsers = async () => {
+      const response = await axiosJWT.get('http://localhost:5000/users', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      // console.log(response.data)
+      setUsers(response.data);
+    }
+  
   return (
     <div className='container mt-5'>
       <h1>Welcome Back: {name}</h1>
@@ -87,7 +83,6 @@ const Dashboard = () => {
         </tbody>
       </table>
     </div>
-  );
-};
-
-export default Dashboard;
+  )
+}
+export default Dashboard
